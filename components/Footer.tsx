b604ca2cd2a1863ba78869ecd1e576878f4c1a07
taiki-fw <taiki_52@outlook.jp>
2020-07-13 23:00:11 +0900

    :sparkles:add main Component
    Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        Next.js app with a <a href="https://snipcart.com">Snipcart</a> - powerd
+        store
+      </p>
+    </footer>
+  );
+};
